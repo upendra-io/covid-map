6a64085d99a0a30df73dd3ae7ac16b029c8e585a
upendraiscookan@gmial.com <upendraiscookan@gmail.com>
2020-09-03 01:03:11 +0530

    🗺  map with leaflet

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Map, Marker, Popup, TileLayer } from "react-leaflet";
+import React from "react";
+import { Icon } from "leaflet";
+import "./App.css";
 
 function App() {
+  const position = [51.505, -0.09];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Map center={position} zoom={3}>
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.osm.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={position}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </Map>
   );
 }
 
