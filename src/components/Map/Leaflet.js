007fb01c0fd6776feae582f312fb59c33ca186c7
upendraiscookan@gmial.com <upendraiscookan@gmail.com>
2020-09-04 16:43:07 +0530

    Mapped cases with countries ❗

--- src/components/Map/Leaflet.js
+++ src/components/Map/Leaflet.js
@@ -1,25 +1,38 @@
-import { Map, Marker, Popup, TileLayer, ZoomControl } from "react-leaflet";
-import React from "react";
+import { Map, TileLayer, ZoomControl } from "react-leaflet";
+import React, { useRef, useEffect } from "react";
 import "./Leaflet.css";
-import mapEffect from "./../../data/countries";
-
-mapEffect();
+import mapEffect from "../../data/countries";
 
 const position = [51.505, -0.09];
 
 export const Leaflet = () => {
+  const mapRef = useRef();
+
+  useRefEffect({
+    ref: mapRef,
+    effect: mapEffect,
+  });
+
+  const mapSettings = {
+    center: position,
+    defaultBaseMap: "OpenStreetMap",
+    zoom: 3,
+    mapEffect,
+  };
+
   return (
-    <Map center={position} zoom={3}>
+    <Map ref={mapRef} {...mapSettings}>
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.osm.org/{z}/{x}/{y}.png"
       />
       <ZoomControl position="bottomright" />
-      <Marker position={position}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
     </Map>
   );
+};
+
+const useRefEffect = ({ effect, ref = {} }) => {
+  useEffect(() => {
+    effect(ref.current);
+  }, [effect, ref]);
 };
--- src/data/countries.js
+++ src/data/countries.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import L from "leaflet";
 
 async function mapEffect({ leafletElement: map } = {}) {
   let response;
@@ -11,6 +12,71 @@ async function mapEffect({ leafletElement: map } = {}) {
 
   const { data = {} } = response;
 
-  console.log(data);
+  const hasData = Array.isArray(data) && data.length > 0;
+
+  if (!hasData) return;
+
+  const geoJson = {
+    type: "FeatureCollection",
+    features: data.map((country = {}) => {
+      const { countryInfo = {} } = country;
+      const { lat, long: lng } = countryInfo;
+      return {
+        type: "Feature",
+        properties: {
+          ...country,
+        },
+        geometry: {
+          type: "Point",
+          coordinates: [lng, lat],
+        },
+      };
+    }),
+  };
+  const geoJsonLayers = new L.GeoJSON(geoJson, {
+    pointToLayer: (feature = {}, position) => {
+      const { properties = {} } = feature;
+      let updatedFormatted;
+      let casesString;
+
+      const { country, updated, cases, deaths, recovered } = properties;
+
+      casesString = `${cases}`;
+
+      if (cases > 1000000) {
+        casesString = `${casesString.slice(0, -6)}M+`;
+      } else if (cases > 1000) {
+        casesString = `${casesString.slice(0, -3)}K+`;
+      }
+      if (updated) {
+        updatedFormatted = new Date(updated).toLocaleString();
+      }
+
+      const html = `
+        <span class="marker">
+          <span class="marker-tooltip">
+            <h2>${country}</h2>
+            <ul>
+              <li><strong>Confirmed:</strong> ${cases}</li>
+              <li><strong>Deaths:</strong> ${deaths}</li>
+              <li><strong>Recovered:</strong> ${recovered}</li>
+              <li><strong>Last Update:</strong> ${updatedFormatted}</li>
+            </ul>
+          </span>
+          ${casesString}
+        </span>
+      `;
+
+      return L.marker(position, {
+        icon: L.divIcon({
+          className: "icon",
+          html,
+        }),
+        riseOnHover: true,
+      });
+    },
+  });
+
+  geoJsonLayers.addTo(map);
 }
 export default mapEffect;
